8530a93b9adf14c5248a5bcbb8ccbac1180027d6
asonkes <audrey.sonkes@gmail.com>
2025-12-09 14:00:22 +0100

    partie contact html + js refactoring ok

--- js/contact/contact.js
+++ js/contact/contact.js
@@ -7,157 +7,65 @@ import { contactInformations } from "/js/utils/jsonFunction.js";
 export function contact() {
   /** Récupérer les variables dont on a besoin */
   const form = document.getElementById("form");
+  if(!form) return;
+
   let input_name = document.getElementById("name");
   let input_lastName = document.getElementById("lastname");
   let input_email = document.getElementById("email");
   let input_password = document.getElementById("password");
   let textarea_message = document.getElementById("message");
-
-  /** On va vider les input d'abord */
-  let isValid = true;
+  /** On fait un tableau des champs du formulaire */
+  const contactFields = [
+    {item: input_name, cls: "errorName", message: "Votre nom est manquant !" },
+    {item: input_lastName, cls: "errorLastname", message: "Votre prénom est manquant !"},
+    {item: input_email, cls: "errorEmail", message: "E-mail non valide !"},
+    {item: input_password, cls: "errorPassword", message: "Votre mot de passe est manquant !" },
+    {item: textarea_message, cls: "errorMessage", message: "Votre message est manquant !"}
+  ]
 
   /** On met 'async' pour faire fonctionner le 'await' */
-  if(form) {
-    form.addEventListener("submit", async (event) => {
+  form.addEventListener("submit", async (event) => {
     event.preventDefault();
 
-    /** Ici si l'input n'est plus vide... */
-    //let errors = document.querySelectorAll('.error')
+    /** On va vider les input d'abord */
+    let isValid = true;
 
-    if (input_name.value != "") {
-      let errorName = document.querySelector(".errorName");
-      if (errorName) {
-        errorName.remove();
-      }
+    /** On supprime le message d'erreur existant */
+    /** cls = class */
+    const removeError = (cls) => {
+      const err = document.querySelector(`.${cls}`);
+      if(err) err.remove();
     }
 
-    if (input_lastName.value != "") {
-      let errorLastname = document.querySelector(".errorLastname");
-      if (errorLastname) {
-        errorLastname.remove();
-      }
-    }
-
-    if (input_email.value != "") {
-      let errorEmail = document.querySelector(".errorEmail");
-      if (errorEmail) {
-        errorEmail.remove();
-      }
-    }
-
-    if (input_password.value != "") {
-      let errorPassword = document.querySelector(".errorPassword");
-      if (errorPassword) {
-        errorPassword.remove();
-      }
-    }
-
-    if (textarea_message.value != "") {
-      let errorTextarea = document.querySelector(".errorTextarea");
-      if (errorTextarea) {
-        errorTextarea.remove();
-      }
-    }
-
-    if (textarea_message.value != "") {
-      let errorMessage = document.querySelector(".errorMessage");
-      if (errorMessage) {
-        errorMessage.remove();
-      }
-    }
-
-    /** Ici on va vérifier si les champs sont vides et ajouter le message d'erreur */
-    // let inputs = document.querySelectorAll('input')
-    // pour chaque input s'il est vide créer span et ajouter
-    // errors = {
-    //     "name" : {
-    //           "class" : "errorName",
-    //           "text" : "Votre nom est manquant !"
-    //      },
-    //      "lastname" : pareil
-    // }
-
-    if (input_name.value === "") {
+    /** On va créer le message d'erreur s'il n'existe pas */
+    const showError = (item, cls, message) => {
       isValid = false;
 
-      let errorName = document.querySelector(".errorName");
+      let err = document.querySelector(`.${cls}`);
 
-      if (!errorName) {
-        errorName = document.createElement("span");
-        errorName.classList.add("error", "errorName");
-        errorName.textContent = `Votre nom est manquant !`;
-        input_name.insertAdjacentElement("beforebegin", errorName);
+      if(!err) {
+        err = document.createElement("span");
+        err.classList.add("error", cls);
+        err.textContent = message;
+        item.insertAdjacentElement("beforebegin", err);
       }
     }
 
-    if (input_lastName.value === "") {
-      isValid = false;
-
-      let errorLastname = document.querySelector(".errorLastname");
-
-      if (!errorLastname) {
-        errorLastname = document.createElement("span");
-        errorLastname.classList.add("error", "errorLastname");
-        errorLastname.textContent = `Votre prénom est manquant !`;
-        input_lastName.insertAdjacentElement("beforebegin", errorLastname);
+    contactFields.forEach(element => {
+      if(element.item.value != "") {
+        removeError(element.cls);
+      } else {
+        showError(element.item, element.cls, element.message);
       }
-    }
-
-    if (input_email.value === "") {
-      isValid = false;
-
-      let errorEmail = document.querySelector(".errorEmail");
-      if (!errorEmail) {
-        errorEmail = document.createElement("span");
-        errorEmail.classList.add("error", "errorEmail");
-        errorEmail.textContent = `E-mail non valide !`;
-        input_email.insertAdjacentElement("beforebegin", errorEmail);
-      }
-    }
-
-    if (input_password.value === "") {
-      isValid = false;
-
-      let errorPassword = document.querySelector(".errorPassword");
-      if (!errorPassword) {
-        errorPassword = document.createElement("span");
-        errorPassword.classList.add("error", "errorPassword");
-        errorPassword.textContent = `Votre mot de passe est manquant !`;
-        input_password.insertAdjacentElement("beforebegin", errorPassword);
-      }
-    }
-
-    if (textarea_message.value === "") {
-      let errorMessage = document.querySelector(".errorMessage");
-      if (!errorMessage) {
-        isValid = false;
-
-        errorMessage = document.createElement("span");
-        errorMessage.classList.add("error", "errorMessage");
-        errorMessage.textContent = `Votre message est manquant !`;
-        textarea_message.insertAdjacentElement("beforebegin", errorMessage);
-      }
-    }
+    });
 
     /** Définir la longueur pour le textarea */
-    let errorMessage = document.querySelector(".errorMessage");
-    if (textarea_message.value.length > 255) {
-      errorMessage.remove();
-
-      isValid = false;
-
-      let errorTextarea = document.querySelector(".errorTextarea");
-      if (!errorTextarea) {
-        errorTextarea = document.createElement("span");
-        errorTextarea.classList.add("error", "errorTextarea");
-        errorTextarea.textContent = `Votre message ne peut dépasser 255 caractères.`;
-        textarea_message.insertAdjacentElement("beforebegin", errorTextarea);
-      }
+    if(textarea_message.value.length > 255) {
+      showError(textarea_message, "errorTextarea", "Votre message ne peut dépasser 255 caractères.");
     } else {
-      let errorTextarea = document.querySelector(".errorTextarea");
-      if (errorTextarea) errorTextarea.remove();
+      removeError("errorTextarea");
     }
-
+    
     /** Si tout est valide, on envoie les données */
     if (isValid) {
       /** On va récupérer les données de mon formulaires, les mettre dans un objet JS */
@@ -178,7 +86,6 @@ export function contact() {
       console.log("Message envoyé !");
     }
   });
-  }
 }
 
 contact();
