27e8caa58193049f744475a999dad89fc8ed2b33
Audrey Sonkes <a.sonkes@interface3.be>
2026-01-16 16:59:21 +0100

    mise à jour

--- js/partials/dark_light.js
+++ js/partials/dark_light.js
@@ -9,48 +9,31 @@ export function darkLight() {
   const light = document.getElementById("light");
   const body = document.querySelector("body");
 
-  /** Lien pour voir quel mode l'utilisateur a choisit sur le serveur */
+  /**
+   * Lien pour voir quel mode l'utilisateur a choisit sur le navigateur
+   * Pas avec le JS
+   * */
   const prefersDark = window.matchMedia("(prefers-color-scheme: dark)");
   console.log(prefersDark);
 
-  if (!prefersDark.matches) {
-    /** Ici on a choisit le mode clair */
-    console.log("mode clair");
+  /*  if (!prefersDark.matches) */
+  /** Maintenant détecté par JS quel mode on a choisit et là enlever ou mettre la classe sur le body !!!  */
 
-    dark.addEventListener("click", (event) => {
-      event.preventDefault();
+  dark.addEventListener("click", (event) => {
+    event.preventDefault();
 
-      dark.classList.add("active");
-      light.classList.add("active");
+    dark.classList.add("active");
+    light.classList.add("active");
 
-      body.classList.add("dark");
-    });
+    body.classList.add("dark");
+  });
 
-    light.addEventListener("click", (event) => {
-      event.preventDefault();
+  light.addEventListener("click", (event) => {
+    event.preventDefault();
 
-      dark.classList.remove("active");
-      light.classList.remove("active");
+    dark.classList.remove("active");
+    light.classList.remove("active");
 
-      body.classList.remove("dark");
-    });
-  } else {
-    /** Ici on a choisit le mode sombre */
-    console.log("sombre");
-
-    light.addEventListener("click", (event) => {
-      console.log("j'ai cliqué sur le bouton");
-      event.preventDefault();
-
-      dark.classList.add("active");
-      light.classList.add("active");
-    });
-
-    dark.addEventListener("click", (event) => {
-      event.preventDefault();
-
-      dark.classList.remove("active");
-      light.classList.remove("active");
-    });
-  }
+    body.classList.remove("dark");
+  });
 }
