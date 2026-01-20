ae42db1f134e82d8f4e10ae741c4f66d167a4fb6
asonkes <audrey.sonkes@gmail.com>
2026-01-20 22:58:11 +0100

    probleme JS - toujours pas résolu

--- js/partials/dark_light.js
+++ js/partials/dark_light.js
@@ -11,29 +11,24 @@ export function darkLight() {
   /** On récupère le mode choisit par l'utilisateur */
   const prefersDark = window.matchMedia("(prefers-color-scheme: dark)");
 
-  /** Fonction pour le mode sombre */
-  const darkLight = body.className;
-  console.log(darkLight);
-
   /** Fonction pour le mode clair */
   const lightMode = () => {
     body.classList.remove("dark");
     dark.classList.remove("active");
     light.classList.remove("active");
-    localStorage.setItem("modeServeurChoose", body.className);
   };
 
   const darkMode = () => {
     body.classList.add("dark");
     dark.classList.add("active");
     light.classList.add("active");
-    localStorage.setItem("myCat", "Tom");
   };
 
   /** On récupère l'information au chargement */
   window.addEventListener("load", () => {
     if (prefersDark.matches) {
       darkMode();
+      localStorage.setItem("modeServeurChoose", "dark");
     }
   });
 
@@ -41,17 +36,21 @@ export function darkLight() {
   prefersDark.addEventListener("change", (event) => {
     if (event.matches) {
       darkMode();
+      localStorage.setItem("modeServeurChoose", "dark");
     } else {
       lightMode();
+      localStorage.setItem("modeServeurChoose", "");
     }
   });
 
   /** Réaction au click sur les icônes */
   dark.addEventListener("click", () => {
     darkMode();
+    localStorage.setItem("modeServeurChoose", "dark");
   });
 
   light.addEventListener("click", () => {
     lightMode();
+    localStorage.setItem("modeServeurChoose", "");
   });
 }
