3d3b6e9f5796a9e670d30382165ab96f76e540c4
Audrey Sonkes <a.sonkes@interface3.be>
2026-01-20 13:26:48 +0100

    modifications thème light/dark

--- js/partials/dark_light.js
+++ js/partials/dark_light.js
@@ -8,45 +8,50 @@ export function darkLight() {
   const dark = document.getElementById("dark");
   const light = document.getElementById("light");
   const body = document.querySelector("body");
-
   /** On récupère le mode choisit par l'utilisateur */
   const prefersDark = window.matchMedia("(prefers-color-scheme: dark)");
 
-  /** Mode clair */
-  if(!prefersDark) {
-    body.classList.remove("dark");
-  } else {
-    /** Par défaut je mets le mode 'sombre' sans évènement au click */
-    body.classList.add("dark");
-    /** On doit changer les icônes du menu header */
-    dark.classList.add("active");
-    light.classList.add("active");
-  }
+  /** Fonction pour le mode sombre */
+  const darkLight = body.className;
+  console.log(darkLight);
 
-  /** Réaction au changement du système (sans reload) */
-  prefersDark.addEventListener("change", (event) => {
-    if(event.maches) {
-      body.classList.add("dark");
-      dark.classList.add("active");
-      light.classList.add("active");
-    } else {
-      body.classList.remove("dark");
-      /** On doit changer les icônes du menu header */
-      dark.classList.remove("active");
-      light.classList.remove("active");
-    }
-  });
-
-  /** Réaction au click */
-  dark.addEventListener("click", () => {
-    body.classList.add("dark");
-    dark.classList.add("active");
-    light.classList.add("active");
-  });
-
-  light.addEventListener("click", () => {
+  /** Fonction pour le mode clair */
+  const lightMode = () => {
     body.classList.remove("dark");
     dark.classList.remove("active");
     light.classList.remove("active");
+    localStorage.setItem("modeServeurChoose", body.className);
+  };
+
+  const darkMode = () => {
+    body.classList.add("dark");
+    dark.classList.add("active");
+    light.classList.add("active");
+    localStorage.setItem("myCat", "Tom");
+  };
+
+  /** On récupère l'information au chargement */
+  window.addEventListener("load", () => {
+    if (prefersDark.matches) {
+      darkMode();
+    }
+  });
+
+  /** Réaction si on a déjà chargé sa page et qu'on change de mode (sombre ou clair) */
+  prefersDark.addEventListener("change", (event) => {
+    if (event.matches) {
+      darkMode();
+    } else {
+      lightMode();
+    }
+  });
+
+  /** Réaction au click sur les icônes */
+  dark.addEventListener("click", () => {
+    darkMode();
+  });
+
+  light.addEventListener("click", () => {
+    lightMode();
   });
 }
